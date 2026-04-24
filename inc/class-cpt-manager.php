007075535e2a444f11a142e5b918f10f3097d493
prabhathAmila <prabhath.amila@gmail.com>
2026-04-24 11:23:06 +0530

    Add Villas and Suites CPTs and refine registration logic

--- inc/class-cpt-manager.php
+++ inc/class-cpt-manager.php
@@ -4,7 +4,6 @@ if (!defined('ABSPATH'))
 
 class PT_CPT_Manager
 {
-
     public function __construct()
     {
         add_action('init', [$this, 'register_pt_cpts']);
@@ -12,25 +11,37 @@ class PT_CPT_Manager
 
     public function register_pt_cpts()
     {
-        // Define our types
+        // Define our types: Headers/Footers are templates, others are public content
         $types = [
             'pt_header' => 'Headers',
             'pt_footer' => 'Footers',
-            'pt_room' => 'Rooms'
+            'pt_room' => 'Rooms',
+            'pt_villa' => 'Villas',
+            'pt_suite' => 'Suites'
         ];
 
         foreach ($types as $slug => $label) {
+            // Determine if this is a template (not public) or a content page (public)
+            $is_template = ($slug === 'pt_header' || $slug === 'pt_footer');
+
             register_post_type($slug, [
                 'labels' => [
                     'name' => $label,
                     'singular_name' => rtrim($label, 's')
                 ],
                 'public' => true,
-                'has_archive' => false,
-                'publicly_queryable' => ($slug === 'pt_room'), // Rooms are public, Headers/Footers are templates
-                'show_in_menu' => 'edit.php?post_type=page', // Or your custom menu
-                'supports' => ['title', 'editor', 'elementor'],
-                'show_in_rest' => true,
+                'has_archive' => !$is_template, // Archives only for content, not templates
+                'publicly_queryable' => !$is_template, // Disable public URLs for templates
+                'show_in_menu' => 'edit.php?post_type=page', // You can change this to a custom menu slug
+                'menu_icon' => $is_template ? 'dashicons-layout' : 'dashicons-admin-home',
+                'supports' => [
+                    'title',
+                    'editor',
+                    'elementor',
+                    'thumbnail',
+                    'excerpt'
+                ],
+                'show_in_rest' => true, // Required for Block Editor & Elementor stability
             ]);
         }
     }
